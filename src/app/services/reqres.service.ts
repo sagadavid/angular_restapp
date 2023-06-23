cc1cfe47d8f59aaeac6f07b9fd9ed1dfecebb341
sagadavid <epostsaga@gmail.com>
2023-06-23 09:19:01 +0200

    inmemoryapi, httpclientmodule, getUsers at home

--- src/app/services/reqres.service.ts
+++ src/app/services/reqres.service.ts
@@ -7,14 +7,18 @@ import { User } from '../user';
   providedIn: 'root',
 })
 export class ReqresService {
+  //base of out endpoints
   private url = 'api/users';
 
+  //we need http client to carry out requests
   constructor(private http: HttpClient) {}
 
+  //list of users
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.url);
   }
 
+  //url by id
   getUser(id: number): Observable<User> {
     const url = `${this.url}/${id}`;
 
