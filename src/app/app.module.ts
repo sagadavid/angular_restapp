044575e59034adb2e5d42d08664832c82e4fd9e0
sagadavid <epostsaga@gmail.com>
2023-06-24 09:30:47 +0200

    userdtaile viewed by activatedRoute &reqresService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './services/in-memory-data.service';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
@@ -12,6 +10,8 @@ import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UserDetailComponent } from './components/user/user-detail.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app.routes';
 
 @NgModule({
   declarations: [
--- src/app/components/user/user-detail.component.ts
+++ src/app/components/user/user-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ReqresService } from '../../services/reqres.service';
 import { User } from '../../user';
@@ -9,26 +9,23 @@ import { User } from '../../user';
   styleUrls: ['./user-detail.component.css'],
 })
 export class UserDetailComponent {
-  constructor(
-    //to access id prameter
-    private activateRoute: ActivatedRoute,
-    private reqresService: ReqresService
-  ) {
-    //initialize property in constructor
-    /** ActivatedRoute is an interface that contains information on a route associated with a component. The “params” property is an Observable, and as such we can subscribe to it.
-     * The observable, if executed successfully, will return a function whose argument is an array of all received parameters.
-     * Then, it is a matter of using our service to obtain the information of the selected user, remembering at all times that our service will always return an Observable because it is executed asynchronously. */
-    this.activateRoute.params.subscribe((params) => {
-      reqresService
-        .getUser(params['id'])
-        .subscribe((res: User) => (this.user = res));
-    });
-  }
-
   user: User = {
     id: 0,
     first_name: '',
     last_name: '',
     avatar: '',
   };
+
+  constructor(
+    //to access id prameter
+    private activatedRoute: ActivatedRoute,
+    private reqresService: ReqresService
+  ) {
+    //initialize property in constructor
+    this.activatedRoute.params.subscribe((params) => {
+      reqresService
+        .getUser(params['id'])
+        .subscribe((res: User) => (this.user = res));
+    });
+  }
 }
